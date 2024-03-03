a352904f937ab26d570816be8b89a6f34cbb70d5
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-03-03 17:13:15 +0100

    implement perRequest memory

--- src/Facades/Forex.php
+++ src/Facades/Forex.php
@@ -5,7 +5,10 @@ namespace Finller\Forex\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static array<string, array<string, int|float>> getRates()
  * @method static array<string, int|float> get(string $currency)
+ * @method static array<string, int|float> query(string $currency)
+ * @method static array<string, int|float> refresh(string $currency)
  *
  * @see \Finller\Forex\Forex
  */
--- src/Forex.php
+++ src/Forex.php
@@ -5,13 +5,48 @@ namespace Finller\Forex;
 class Forex
 {
     /**
+     * @var array<string, array<string, int|float>>
+     */
+    protected array $rates = [];
+
+    /**
      * @return array<string, int|float>
      */
     public function get(string $currency): array
+    {
+        if (array_key_exists($currency, $this->rates)) {
+            return $this->rates[$currency];
+        }
+
+        return $this->refresh($currency);
+    }
+
+    /**
+     * @return array<string, int|float>
+     */
+    public function query(string $currency): array
     {
         /** @var string $request */
         $request = config('forex.request');
 
         return $request::get($currency);
+    }
+
+    /**
+     * @return array<string, int|float>
+     */
+    public function refresh(string $currency): array
+    {
+        $this->rates[$currency] = $this->query($currency);
+
+        return $this->rates[$currency];
+    }
+
+    /**
+     * @return array<string, array<string, int|float>>
+     */
+    public function getRates(): array
+    {
+        return $this->rates;
     }
 }
